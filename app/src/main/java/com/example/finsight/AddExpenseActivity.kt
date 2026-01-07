9be34cf61ba018531a9ef3acc9ac33e7120b6ced
Mia Strinic <mstrin01@fesb.hr>
2026-01-07 13:06:44 +0100

    Add expense button + navigation from Home to Add Expense

---
+++ app/src/main/java/com/example/finsight/AddExpenseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finsight
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddExpenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_expense)
+    }
+}
--- app/src/main/java/com/example/finsight/HomeActivity.kt
+++ app/src/main/java/com/example/finsight/HomeActivity.kt
@@ -1,12 +1,22 @@
 package com.example.finsight
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val addButton = findViewById<Button>(R.id.btnAddExpense)
+
+        addButton.setOnClickListener {
+            val intent = Intent(this,AddExpenseActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
